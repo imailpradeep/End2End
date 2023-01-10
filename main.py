8a0a938a27ede94474fb1d584af3d333315808a1
PBhaskar <imailpradeep1@gmail.com>
2023-01-10 11:39:52 +0530

    added catboost in requirements.txt

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 from data_dump import upload_data_to_mongoDB
 from utils import get_data_from_MongoDB
 
-
+# upload data from market_basket folder to MongoDB inorder to replicate industry standards
 upload_data_to_mongoDB()
+
+# download data from MongoDB
 df_list = get_data_from_MongoDB()
 
 
